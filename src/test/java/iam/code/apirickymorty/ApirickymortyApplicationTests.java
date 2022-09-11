37a7e5530f1aad5d7440e33fe3d22779be15dc61
Ignacio Avila Montero <ignacio.avila@bluetab.net>
2022-09-11 17:43:14 +0200

    init testing

--- src/test/java/iam/code/apirickymorty/ApirickymortyApplicationTests.java
+++ src/test/java/iam/code/apirickymorty/ApirickymortyApplicationTests.java
@@ -1,13 +1,78 @@
 package iam.code.apirickymorty;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = ApirickymortyApplication.class)
+@AutoConfigureMockMvc
 class ApirickymortyApplicationTests {
 
+    // TEST API REST
+
     @Test
-    void contextLoads() {
+    public void findCharacter() throws Exception {
+        assertThat(false).isTrue();
     }
 
+    @Test
+    public void dontFindCharacter() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    // TEST EPISODES APPLICATION
+
+    @Test
+    public void searchEpisodesWithNullCharacter() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchEpisodesWithEmptyList() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchEpisodesWithListWithOneElement() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchEpisodesWithListWithMoreThanOneElement() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchEpisodesWithListWithMoreThanOneElementAndOneNotExists() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    // TEST CHARACTER APPLICATION
+
+    @Test
+    public void searchCharacterWithNameNull() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchCharacterWithNameEmpty() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchCharacterWithNameEmptyPlus() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchCharacterWithNameRick() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    public void searchCharacterWithInventedName() throws Exception {
+        assertThat(false).isTrue();
+    }
 }
