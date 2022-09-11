d33566838a1fbc45353a3fac4d16a36994ce9e67
Ignacio Avila Montero <ignacio.avila@bluetab.net>
2022-09-11 20:35:41 +0200

    add Swagger API Documentation

--- src/main/java/iam/code/apirickymorty/app/infraestructure/ApiRickMortyConfig.java
+++ src/main/java/iam/code/apirickymorty/app/infraestructure/ApiRickMortyConfig.java
@@ -4,10 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.Locale;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
+@EnableSwagger2
 public class ApiRickMortyConfig {
     @Bean
     public LocaleResolver localeResolver() {
@@ -15,4 +24,15 @@ public class ApiRickMortyConfig {
         slr.setDefaultLocale(Locale.ENGLISH);
         return slr;
     }
+
+    @Bean
+    public Docket apiv10() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("api-rick-y-morty-1.0")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
